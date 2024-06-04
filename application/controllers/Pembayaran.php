9e70838e9c631fdc0066c031d52957606db49952
oalahbro <epradana15@gmail.com>
2024-06-05 00:18:39 +0700

    penambahan bukti bayar

--- application/controllers/Pembayaran.php
+++ application/controllers/Pembayaran.php
@@ -9,6 +9,7 @@ class Pembayaran extends MY_Controller
 
         $this->load->model('M_pembayaran');
         $this->load->library('form_validation');
+        $this->load->library('upload');
     }
     public function index()
     {
@@ -30,13 +31,33 @@ class Pembayaran extends MY_Controller
     {
         $userId = $this->session->userdata('user')->id;
 
+        $config['upload_path']          = './assets/uploads/';
+        $config['allowed_types']        = 'gif|jpg|png';
+        $config['max_size']             = 2048; // ukuran maksimal file dalam KB
+
+        $file_ext = pathinfo($_FILES['bukti_pembayaran']['name'], PATHINFO_EXTENSION);
+        // Generate file name dengan timestamp
+        $config['file_name'] = time() . '.' . $file_ext;
+
+        $this->upload->initialize($config);
+
+        if (!$this->upload->do_upload('bukti_pembayaran')) {
+            $error = array('error' => $this->upload->display_errors());
+            // $this->load->view('upload_form', $error);
+            var_dump($error);
+        } else {
+            $img = array('upload_data' => $this->upload->data());
+            // $this->load->view('upload_success', $data);
+            var_dump($img['upload_data']['file_name']);
+        }
         $data = [
             'id_member' => $userId,
             'tgl_bayar' => $this->input->post('paymentDate'),
             'total_bayar' => $this->input->post('paymentAmount'),
-            'status' => 0
+            'status' => 0,
+            'notes' => 0,
+            'bukti_bayar' => $img['upload_data']['file_name']
         ];
-
         if ($this->M_pembayaran->savePayment($data)) {
             $this->session->set_flashdata('message', 'Pembayaran berhasil disimpan.');
         } else {
--- application/views/pembayaran.php
+++ application/views/pembayaran.php
@@ -19,7 +19,7 @@
     <div class="card mb-4">
         <div class="card-header">Form Pembayaran</div>
         <div class="card-body">
-            <form id="paymentForm" action="<?php echo site_url('Pembayaran/submitPayment'); ?>" method="post">
+            <form id="paymentForm" enctype="multipart/form-data" action="<?php echo site_url('Pembayaran/submitPayment'); ?>" method="post">
                 <div class="mb-3">
                     <label for="paymentDate" class="form-label">Tanggal Pembayaran</label>
                     <input type="date" class="form-control" id="paymentDate" name="paymentDate" value="<?php echo date('Y-m-d'); ?>" required>
@@ -30,7 +30,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="buktipembayaran" class="form-label">Bukti Pembayaran</label>
-                    <input type="file" class="form-control" id="buktipembayaran" name="buktipembayaran" placeholder="Masukkan nominal pembayaran" required>
+                    <input type="file" class="form-control" id="buktipembayaran" name="bukti_pembayaran" placeholder="Masukkan nominal pembayaran" required>
                 </div>
                 <button type="submit" class="btn btn-primary">Bayar</button>
             </form>
@@ -47,6 +47,7 @@
                         <th scope="col">Tanggal</th>
                         <th scope="col">Nominal</th>
                         <th scope="col">Status</th>
+                        <th scope="col">Bukti Bayar</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -61,10 +62,48 @@
                                     <button class="btn btn-warning">Lunas</button>
                                 <?php endif; ?>
                             </td>
+                            <td>
+                                <button class="btn btn-link bukti-bayar" data-image-url="<?php echo base_url('assets/uploads/' . $payment->bukti_bayar); ?>">
+                                    Lihat Bukti
+                                </button>
+                            </td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
     </div>
+
+    <div class="modal fade" id="imageModal" tabindex="-1" aria-labelledby="imageModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="imageModalLabel">Preview Bukti Bayar</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <img id="previewImage" src="" class="img-fluid" alt="Bukti Bayar">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        var buktiBayarElements = document.querySelectorAll('.bukti-bayar');
+
+        buktiBayarElements.forEach(function(element) {
+            element.addEventListener('click', function() {
+                var imageUrl = this.getAttribute('data-image-url');
+                var modalImage = document.getElementById('previewImage');
+                modalImage.src = imageUrl;
+                var imageModal = new bootstrap.Modal(document.getElementById('imageModal'));
+                imageModal.show();
+            });
+        });
+    });
+</script>
