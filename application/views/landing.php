8a81c8bda4e2f3fab598c693bf6d54f86d83ade0
oalahbro <epradana15@gmail.com>
2024-07-02 22:14:47 +0700

    fixing landing page

--- application/views/landing.php
+++ application/views/landing.php
@@ -1,23 +1,24 @@
 <!-- untuk home -->
-<section id="home">
-    <img src="<?php echo $halaman[1]['gambar']; ?>" style="height:375px; width:500px" />
-    <div class="kolom">
+<section id="home" class="gap-4 mt-4">
+    <img src=" <?php echo $halaman[1]['gambar']; ?>" style="max-width:500px" class="rounded-4" />
+
+    <div class="my-auto">
         <p class="deskripsi"><?php echo $halaman[1]['kutipan'] ?></p>
         <h2><?php echo $halaman[1]['judul'] ?></h2>
-        <?php echo $halaman[1]['isi'] ?>
+        <p><?php echo $halaman[1]['isi'] ?> </p>
         <p><a href="<?php echo $halaman[1]['url'] ?>" class="tbl-pink">Pelajari Lebih Lanjut</a></p>
     </div>
 </section>
 
 <!-- untuk courses -->
-<section id="courses">
-    <div class="kolom">
+<section id="courses" class="gap-4">
+    <div class="my-auto">
         <p class="deskripsi"><?php echo $halaman[2]['kutipan'] ?></p>
         <h2><?php echo $halaman[2]['judul'] ?></h2>
-        <?php echo $halaman[2]['isi'] ?>
+        <p><?php echo $halaman[2]['isi'] ?></p>
         <p><a href="<?php echo $halaman[2]['url'] ?>" class="tbl-biru">Pelajari Lebih Lanjut</a></p>
     </div>
-    <img src="<?php echo $halaman[2]['gambar'] ?>" />
+    <img src="<?php echo $halaman[2]['gambar'] ?>" style="max-width:500px" class="rounded-4" />
 </section>
 
 <section id="tutors">
