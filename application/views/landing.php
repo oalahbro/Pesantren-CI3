77a26f11e02a243f7ade6a86aa727673e128c9e0
Syahrul0123 <msyahrul725@gmail.com>
2024-07-22 23:26:57 +0700

    ok

--- application/views/landing.php
+++ application/views/landing.php
@@ -53,7 +53,6 @@
 <section id="partners">
     <div class="tengah">
         <div class="kolom">
-            <p class="deskripsi">Our Top Partners</p>
             <h2>Partners</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi magni tempore expedita sequi. Similique rerum doloremque impedit saepe atque maxime.</p>
         </div>
